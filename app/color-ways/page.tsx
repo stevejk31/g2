507d1c7e0862f52b13b0be9241895c80c7f20abf
steve <stevekim31@gmail.com>
2025-04-03 16:38:05 -0700

    add colorway

--- app/color-ways/page.tsx
+++ app/color-ways/page.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import fsPromises from 'fs/promises';
-import path from 'path';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 
+import { fetchColorWays } from '@/app/lib/db/colorWays';
+
 export default async function ColorWaysPage() {
-  const colorWays = await fsPromises.readFile(path.join(process.cwd(), 'backup/color-ways.json'), { encoding: 'utf8' });
-  const colorWaysJson = JSON.parse(colorWays);
+  const colorWays = await fetchColorWays();
   return (
     <div className="w-full p-5">
       <Typography variant="h1" component="h1">Color ways</Typography>
       <ul className="flex items-stretch justify-around flex-wrap gap-7 list-none">
-        {Object.entries(colorWaysJson).map(([name, src]) => (
+        {colorWays.map(({ name, src }) => (
           <li key={name}>
             {name}
             <Image alt={name} src={src as string} width="200" height="200" />
---
+++ app/lib/db/colorWays.ts
@@ -0,0 +1,67 @@
+/* eslint camelcase: off */
+import sql from '@/app/lib/db/sql';
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+/**
+ * @fileoverview
+ *
+ * Access values from color_way table.
+ */
+export interface ColorWayRow {
+  id: number;
+  name: string;
+  src: string;
+}
+
+export interface NewColorWay {
+  name: string;
+  src: string;
+}
+
+export const addColorWays = async (colorWays: NewColorWay[]) => {
+  await sql`
+    INSERT INTO color_way
+    ${sql(colorWays)}
+  `;
+};
+
+export const createColorWayTable = async (doSeed: boolean = false, doDropTable: boolean = false) => {
+  if (doDropTable) {
+    await sql`
+      DROP TABLE IF EXISTS color_way;
+    `;
+
+    await sql`
+      DISCARD ALL;
+    `;
+  }
+
+  await sql`
+    CREATE TABLE IF NOT EXISTS color_way(
+      id SERIAL NOT NULL PRIMARY KEY,
+      name TEXT NOT NULL,
+      src TEXT NOT NULL
+    );
+  `;
+
+  if (doSeed) {
+    const colorWays = await fsPromises.readFile(
+      path.join(process.cwd(), 'backup/color-ways.json'),
+      { encoding: 'utf8' },
+    );
+    const values = Object.entries(JSON.parse(colorWays))
+      .filter(([name, src]) => !!name && !!src)
+      .map(([name, src]) => ({
+        name,
+        src: src as string,
+      }));
+    await addColorWays(values);
+  }
+};
+
+export const fetchColorWays = async () => sql<ColorWayRow[]>`
+    SELECT *
+    FROM color_way
+    ORDER BY name ASC;
+  `;
